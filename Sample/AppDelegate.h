e9c11118e0f57e14510774b5a3ef14236127dfc7
Kim.Seondeok <kim.seondeok@access-company.com>
2014-10-29 13:56:02 +0900

    Initial Commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Kim.Seondeok on 2014/10/29.
+//  Copyright (c) 2014年 Kim.Seondeok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by Kim.Seondeok on 2014/10/29.
+//  Copyright (c) 2014年 Kim.Seondeok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
